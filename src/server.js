588f2ed8d0a134cdd91c00068a90aa03a6648bee
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-19 23:24:50 +0400

    feaT: add swagger middleweare

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import bodyParser from "body-parser";
 import express from "express";
+import swaggerMiddleware from "./middlewares/swagger-middleware";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/", ...swaggerMiddleware);
 
 app.listen(process.env.PORT || 3000);
