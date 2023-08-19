f1e286a5b5a2347e067140e4cd29a54afddc0282
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-20 01:12:22 +0400

    feat: create user verify route & swagger definition

--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/auth-controller.js";
+import { createUser, verifyUser } from "../controllers/auth-controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", createUser);
+authRouter.post("/verify", verifyUser);
 
 export default authRouter;
