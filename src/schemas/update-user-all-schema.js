e67deb779ee5a63f9f2726a53d1032add7d311a7
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-20 08:57:13 +0400

    feat: create user update with multer schema

---
+++ src/schemas/update-user-all-schema.js
@@ -0,0 +1,24 @@
+import Joi from "joi";
+import pool from "../config/sql.js";
+
+const ifUserExist = (user) => (value, helpers) => {
+  if (user) {
+    return helpers.message("user with this email already exist");
+  }
+
+  return value;
+};
+
+export const updateUserAllSchema = async (data) => {
+  const user = await pool.query("SELECT * FROM users WHERE id = $1", [data.id]);
+
+  return Joi.object({
+    email: Joi.string().email().required().trim(),
+    firstName: Joi.string().trim().required(),
+    lastName: Joi.string().trim().required(),
+    avatar: Joi.string().trim().required(),
+    id: Joi.number().custom(ifUserExist(user.rows[0])).required(),
+  });
+};
+
+export default updateUserAllSchema;
