94d6809a5151101fe157cc44ac0aa8b47ece0a8f
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-20 09:34:01 +0400

    feat: create user update without multer request function

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,5 @@
 import pool from "../config/sql.js";
-import updateUserAllSchema from "../schemas/update-user-all-schema.js";
+import updateUserSchema from "../schemas/update-user-schema.js";
 
 export const updateUserWithAvatar = async (req, res) => {
   const { body, params, file } = req;
@@ -10,8 +10,8 @@ export const updateUserWithAvatar = async (req, res) => {
       return res.status(402).json({ message: "image did not upload" });
     }
     const avatar = "/storage/" + file.filename;
-    const validator = await updateUserAllSchema({ ...body, avatar, id });
-    const { value, error } = validator.validate({ ...body, avatar, id });
+    const validator = await updateUserSchema({ ...body, id });
+    const { value, error } = validator.validate({ ...body, id });
 
     if (error) {
       return res.status(401).json(error.details);
@@ -21,6 +21,30 @@ export const updateUserWithAvatar = async (req, res) => {
     await pool.query(
       "UPDATE users SET firstname = $1, lastname = $2, email = $3, image = $4 WHERE id = $5",
       [firstName, lastName, email, avatar, id]
+    );
+
+    return res.status(204).json({ message: "user updated successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { body, params } = req;
+  const { id } = params;
+
+  try {
+    const validator = await updateUserSchema({ ...body, id });
+    const { value, error } = validator.validate({ ...body, id });
+
+    if (error) {
+      return res.status(401).json(error.details);
+    }
+
+    const { firstName, lastName, email } = value;
+    await pool.query(
+      "UPDATE users SET firstname = $1, lastname = $2, email = $3 WHERE id = $4",
+      [firstName, lastName, email, id]
     );
 
     return res.status(204).json({ message: "user updated successfully" });
--- src/schemas/update-user-schema.js
+++ src/schemas/update-user-schema.js
@@ -9,16 +9,15 @@ const ifUserExist = (user) => (value, helpers) => {
   return value;
 };
 
-export const updateUserAllSchema = async (data) => {
+export const updateUserSchema = async (data) => {
   const user = await pool.query("SELECT * FROM users WHERE id = $1", [data.id]);
 
   return Joi.object({
     email: Joi.string().email().required().trim(),
     firstName: Joi.string().trim().required(),
     lastName: Joi.string().trim().required(),
-    avatar: Joi.string().trim().required(),
     id: Joi.number().custom(ifUserExist(user.rows[0])).required(),
   });
 };
 
-export default updateUserAllSchema;
+export default updateUserSchema;
