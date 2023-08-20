827b14742eb03d23bc45c0d9a4a4f798eb83bf19
Nika Nozadze <nikanozadze1010@gmail.com>
2023-08-20 08:50:37 +0400

    feat: add multer & user put request with multer

---
+++ src/routes/user-router.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { updateUserWithAvatar } from "../controllers/user-controller.js";
+
+const userRouter = express.Router();
+
+export const fileStorage = multer.diskStorage({
+  destination: (_, __, callback) => {
+    callback(null, "public/storage");
+  },
+  filename: (_, file, callback) => {
+    const timestamp = new Date().toISOString().replace(/:/g, "-");
+    const name = file.originalname.replace(/[^a-zA-Z0-9.]/g, "_");
+    callback(null, timestamp + name);
+  },
+});
+
+export const fileFilter = (__, file, callback) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+};
+
+userRouter.put(
+  "/users/all",
+  multer({ storage: fileStorage, fileFilter }).single("avatar"),
+  updateUserWithAvatar
+);
+
+export default userRouter;
